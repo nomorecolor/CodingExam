a6a6a9f470027a80b66250b9ad9cb1c0c5c197ba
nomorecolor <alain.elemia@gmail.com>
2021-12-08 00:47:46 +0800

    Add Helpers static class

---
+++ CodingExam.Common/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CodingExam.Common
+{
+    public static class Helpers
+    {
+        public static string Encrypt(string word)
+        {
+            var data = Encoding.ASCII.GetBytes(word);
+            data = new SHA256Managed().ComputeHash(data);
+
+            return Encoding.ASCII.GetString(data);
+        }
+    }
+}
