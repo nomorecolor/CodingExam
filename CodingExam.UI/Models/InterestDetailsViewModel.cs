7630c6b2faa28f07e0bc7bd539182afaa8ef5d88
nomorecolor <alain.elemia@gmail.com>
2021-12-10 21:53:36 +0800

    Add Interest controller, views and models

---
+++ CodingExam.UI/Models/InterestDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CodingExam.UI.Models
+{
+    public class InterestDetailsViewModel
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public decimal PresentValue { get; set; }
+        public decimal InterestRate { get; set; }
+        public decimal FutureValue { get; set; }
+        public int InterestId { get; set; }
+    }
+}
